64d975e49e7c14819da913ef6ea553cde41e6c77
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-09-14 15:49:33 -0300

    Adding first version of complete sudoku description

--- sudoku.pl
+++ sudoku.pl
@@ -23,16 +23,19 @@ GetOptions("help" => \$help);
 if($help) { print while <DATA>; exit; }
 
 # Number of queens
-scalar @ARGV == 1 and my $n_squares = shift @ARGV 
-or die "USAGE: perl sudoku.pl n_squares\n";
+scalar @ARGV == 1 and my $prop = shift @ARGV 
+or die "USAGE: perl sudoku.pl prop\n";
 
-$n_squares =~ /^(\d+)$/
-or die "The number of squares must be made only of integers";
+$prop =~ /^(\d+)$/
+or die "The prop must be made only of integers";
 
-($n_squares = $1) > 0
-or die "The number of squares must be strictly positive";
+($prop = $1) > 0
+or die "The prop must be strictly positive";
 
-# Number of squares and clausules
+# Number of squares
+my $n_squares = $prop**2;
+
+# Number of variables and clausules
 my $n_vars = $n_squares**3;
 my $n_clausules = n_lines($n_squares);
 
@@ -84,6 +87,10 @@ for(my $i = 1; $i <= $n_squares*$n_squares; $i++) # (1..$n_squares)
 
 # Third clausules: just 1 number per subgrid
 # ¬S_i+k,j+k ∨ ¬S_i+l,j+l, ∀ i,j ∈ [1,n], ∀ k ∈ [1,√n], l ∈ [j,√n]
+for(my $y = 1; $y <= $n_squares; $y += $prop) {
+    for(my $x = 1; $x <= $n_squares; $x++) 
+        { &two_by_two_grid($x, $y); }
+}
 
 #######################################################################
 ##                          SUBROUTINES                              ##
@@ -98,8 +105,11 @@ sub n_lines {
     my $n_squares = shift;       # Number of squares
     my $n_lines = $n_squares**2; # 1 Number per position in the grid.
         
-    # Just 1 queen per column
-    $n_lines += $n_squares**2 *($n_squares*($n_squares-1))/2;
+    # Just 1 number per column
+    $n_lines += $n_squares**2 * ($n_squares*($n_squares-1))/2;
+    
+    # Just 1 number per subgrid
+    $n_lines += $prop * $n_squares**2 * ($n_squares*($n_squares-1))/2;
                                            
     return $n_lines;
 }
@@ -108,14 +118,58 @@ sub n_lines {
 # Arguments:   list of numbers
 # Description: Given a a list, prints the elements $a and $b 
 #              in the format "-$a -$b 0".
-sub two_by_two 
+sub two_by_two_grid
 {
-    my $pos = shift; my $size = scalar @{$pos};
-    for(my $i = 0; $i < $size; $i++) {
-        for(my $j = $i+1; $j < $size; $j++) {
-            print "-$pos->[$i] -$pos->[$j] 0\n";
-        }
-    }
+    my  ($x, $y) = (shift, shift);
+    my $position = 1 + ($x-1)*$n_squares + ($y-1)*$n_squares**2;
+    
+    # Goes from $position to $position + 9, for all numbers
+    for my $pos ($position .. $position+($n_squares-1) )
+    {
+        # I: → , J: ↓
+        for(my $i = 0; $i < $prop; $i++) {
+            for(my $j = 0; $j < $prop; $j++)
+            {
+                #                       Goes untill √9-1
+                #            .-----------------------------------.
+                #          x + 0*9                             x + 1*9
+                #            ^                                   ^
+                #            1   2   3   4   5   6   7   8   9   1   ...
+                #          .---.---.---.---.---.---.---.---.---.---. ...
+                # y + 0*9² | A |   |   |   |   |   |   |   |   | D | 
+                #          |---|---|---|---|---|---|---|---|---|---| 
+                # y + 1*9² | B |   |   |   |   |   |   |   |   | E | 
+                #          |---|---|---|---|---|---|---|---|---|---| 
+                # y + 2*9² | C |   |   |   |   |   |   |   |   | F | 
+                #          |---|---|---|---|---|---|---|---|---|---| 
+                #     ^                         ⋮                    
+                #     '-- Goes untill √9-1                          
+                # 
+                # Produces (in this order): 
+                # 
+                # ¬A ∨ ¬B, ¬A ∨ ¬C, ¬A ∨ ¬D, ¬A ∨ ¬E, ¬A ∨ ¬F, ...
+                # ¬B ∨ ¬C, ¬B ∨ ¬D, ¬B ∨ ¬E, ¬B ∨ ¬F, ...
+                # ¬C ∨ ¬D, ¬C ∨ ¬E, ¬C ∨ ¬F, ...
+                # ¬D ∨ ¬E, ¬D ∨ ¬F, ...
+                # ¬E ∨ ¬F, ...
+                # ...
+                
+                # K: → , L: ↓
+                for(my $k = $i; $k < $prop; $k++) {
+                    for(my $l = ($k==$i)? ($j+1):(0); $l < $prop; $l++)
+                    {
+                        # Produces the numbers for the 1st and 2nd arg
+                        my $first  = $pos + ($i * $n_squares)     # → 
+                                          + ($j * $n_squares**2); # ↓ 
+                        my $second = $pos + ($k * $n_squares)     # → 
+                                          + ($l * $n_squares**2); # ↓ 
+                        
+                        print "-$first -$second 0\n";
+                    }
+                } #k
+            } #j
+        } #i
+    } #position
 }
 
 #######################################################################
