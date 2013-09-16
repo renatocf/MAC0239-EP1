a82c12bbf3878d32a62951b40a77a5b6979fc397
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-09-16 19:04:56 -0300

    Adding test version zChaff

--- sudoku.pl
+++ sudoku.pl
@@ -9,12 +9,16 @@ use warnings;
 # Libraries
 use FindBin qw($Bin);
 use lib "$Bin/Sudoku";
+use lib "$Bin/zchaff";
 
 # Packages
 use Sudoku;
+use Zchaff;
 
 my Sudoku $sudoku = new Sudoku(3);
 $sudoku->gen_cnf();
+
+my $sat_solver = Zchaff::init(729);
 
 #######################################################################
 ##                          DOCUMENTATION                            ##
