37641b6ce4fec1f94369a4987f7ab1ed455379f1
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-09-17 16:19:46 -0300

    Adding color printing for the input

--- Sudoku/grid_print.pm
+++ Sudoku/grid_print.pm
@@ -6,6 +6,8 @@ use v5.14;
 use strict;
 use warnings;
 
+use Term::ANSIColor qw(:constants);
+
 # Subroutine:  grid_print
 # Arguments:   none
 # Description: Given the answer stored inside the object, print it
@@ -14,6 +16,8 @@ use warnings;
 sub grid_print 
 {
     my $sudoku = shift;
+    my $prop      = $sudoku->{PROP};
+    my $input     = $sudoku->{SUDOKU};
     my $n_squares = $sudoku->{N_SQUARES};
     my $ANS = $sudoku->solution();
     
@@ -21,26 +25,32 @@ sub grid_print
     say "The problem is $title.";
     $title =~ /^(UN|)SAT$/i or die "Not a minisat answer!";
     exit if($1);
-
+    
     # Prints the top of the table
     print ".---" x $n_squares, ".\n";
-
+    
     # The middle
     $/ = " ";
-    my $i = 0;
+    my ($i, $count, $blocks) = (0, -1, 0);
     while(my $var = <$ANS>)
     {
         if($var > 0)
         {
-            my $res; $i++;
+            my $res; $i++; $count++;
             unless($res = $var % $n_squares) { $res += $n_squares }
-            print "| $res ";
             
+            print "| ";
+            
+            # Print with colors (if avaiable)
+            ($input->[$count]) 
+                ? (print BOLD, RED, "$res ", RESET)
+                : (print "$res ");
+           
             if($i % $n_squares == 0)
             {
                 print "|\n";
-                ($i != $n_squares**2) ?
-                    (print "|---" x $n_squares, "|\n") : ();
+                ($i != $n_squares**2)
+                    ? (print "|---" x $n_squares, "|\n") : ();
             }
         }
     }
