81751b5c390dd03e7e3a5338e7df48fd9368e351
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-09-17 10:25:50 -0300

    Changing sudoku.pl to accept other proportions of sudokus (9x9, 16x16, 25x25, ...)

--- sudoku.pl
+++ sudoku.pl
@@ -11,17 +11,35 @@ use FindBin qw($Bin);
 use lib "$Bin/Sudoku";
 use lib "$Bin/zchaff";
 
-# Packages
+# Classes
 use Sudoku;
 use Zchaff;
 
+#######################################################################
+##                              OPTIONS                              ##
+#######################################################################
+
+# Options
+use Getopt::Long;
+my $help = undef;
+my $prop = 3;
+GetOptions(
+    "help"         => \$help, 
+    "proportion=i" => \$prop,
+);
+
+# Usage
 scalar @ARGV == 1
 or die "USAGE: sudoku.pl input.txt";
+
+#######################################################################
+##                                MAIN                               ##
+#######################################################################
 
 my $filename = shift;
 open(my $fh, "<", $filename);
 
-my Sudoku $sudoku = new Sudoku(3);
+my Sudoku $sudoku = new Sudoku($prop);
 $sudoku->upload     ($fh);
 $sudoku->gen_cnf    ();
 $sudoku->grid_print ();
