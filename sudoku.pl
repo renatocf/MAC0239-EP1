4a7a3807907c9f27bc3e9c48aef5238371393a70
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-09-17 13:07:49 -0300

    Taking out zchaff library

--- sudoku.pl
+++ sudoku.pl
@@ -12,7 +12,6 @@ use lib "$Bin/Sudoku";
 
 # Classes
 use Sudoku;
-use Zchaff;
 
 #######################################################################
 ##                              OPTIONS                              ##
